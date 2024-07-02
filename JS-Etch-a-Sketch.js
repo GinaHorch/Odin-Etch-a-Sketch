35dd8fb425d94f7df4d9a477946869d477ae6b20
Gina Horch <168549659+GinaHorch@users.noreply.github.com>
2024-07-02 16:43:59 +0800

    Added eventListener to button and added a function to clear the grid

--- JS-Etch-a-Sketch.js
+++ JS-Etch-a-Sketch.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('#grid-container');
+    const button = document.querySelector('#new-grid-button');
 
+    const clearGrid = () => {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+    
     const createGrid = (size) => {
         for (let i = 0; i < size * size; i++) {
             const div = document.createElement('div');
