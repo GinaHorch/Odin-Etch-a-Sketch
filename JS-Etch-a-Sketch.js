9f079fe7e880e9e5aa73926b778cb42016381491
Gina Horch <168549659+GinaHorch@users.noreply.github.com>
2024-07-02 17:18:46 +0800

    Adjusted CSS and added style into JS for itemSize to adjust

--- JS-Etch-a-Sketch.js
+++ JS-Etch-a-Sketch.js
@@ -9,14 +9,31 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     const createGrid = (size) => {
+        clearGrid();
+        const gridSize = 400;
+        const itemSize = gridSize / size;
+
         for (let i = 0; i < size * size; i++) {
             const div = document.createElement('div');
             div.classList.add('grid-item');
+            div.style.width = `${itemSize}px`
+            div.style.height = `${itemSize}px`
+            
             div.addEventListener('mouseover', () => {
                 div.classList.add('hovered');
             });
             container.appendChild(div);
     }
 }
+
+button.addEventListener('click', () => {
+    let size = prompt("Enter the number of squares per side to the new grid (max 100):");
+    size = parseInt(size);
+    if (size > 0 && size <= 100) {
+        createGrid(size);
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
+});
 createGrid(16);
 });
